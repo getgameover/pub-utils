83e2fea2fd67b86441b421cfb73cf9b6acaa108d
luqili <luqili2006@126.com>
2016-12-21 17:14:01 +0800

    调整验证功能，及测试用例

--- myutils/src/main/java/com/luqili/utils/idcard/IDCardUtils.java
+++ myutils/src/main/java/com/luqili/utils/idcard/IDCardUtils.java
@@ -1,8 +1,10 @@
 package com.luqili.utils.idcard;
 
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
@@ -11,26 +13,16 @@ import com.luqili.utils.date.DateUtils;
 import com.luqili.utils.district.DistrictUtils;
 
 public class IDCardUtils {
+	public static Integer maxAge = 200;// 最大年龄
+	public static Pattern pattern_idcard = Pattern.compile("^\\d{17}([\\dX]{1})?$");// 验证身份证
+	public static Pattern pattern_creditcode = Pattern.compile("^[0-9ABCDEFGHJKLMNPQRTUWXY]{18}$");// 验证社会信用代码
+	public static Pattern pattern_orgcode = Pattern.compile("^[0-9A-Z]{8}-?[0-9A-Z]{1}$");
+	public static int[] weight_idcard = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1 };
+	public static int[] weight_creditcode = { 1, 3, 9, 27, 19, 26, 16, 17, 20, 29, 25, 13, 8, 24, 10, 30, 28 };
+	public static int[] weight_orgcode = { 3, 7, 9, 10, 5, 8, 4, 2 };
+	public static char[] validValue = { '1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2' };
+	public static char[] tables = "0123456789ABCDEFGHJKLMNPQRTUWXY".toCharArray(); // 字典表
 
-	/**
-	1.将前面的身份证号码17位数分别乘以不同的系数。 从第一位到第十七位的系数分别为：7 9 10 5 8 4 2 1 6 3 7 9 10 5 8 4 2
-	2.将这17位数字和系数相乘的结果相加。
-	3.用加出来和除以11，看余数是多少？
-	4.余数只可能有0 1 2 3 4 5 6 7 8 9 10这11个数字。 其分别对应的最后一位身份证的号码为1 0 X 9 8 7 6 5 4 3 2。
-	5.通过上面得知如果余数是2，就会在身份证的第18位数字上出现罗马数字的Ⅹ。 如果余数是10，身份证的最后一位号码就是2。
-	 */
-	
-	public static Pattern validPattern = Pattern.compile("^\\d{17}([\\dX]{1})?$");//使用打正则表达式
-	public static Integer maxAge=200;//最大年龄
-	public static int[] modeValue = new int[17];
-	public static char[] validValue={'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
-	
-	static{//初始化
-		for(int i=0;i<modeValue.length;i++){
-			modeValue[i]=(1<< (modeValue.length - i))%11;
-		}
-	}
-	
 	/**
 	 * 获取新身份证的最后一位:检验位
 	 * 
@@ -38,65 +30,70 @@ public class IDCardUtils {
 	 *            18位身份证的前17位
 	 * @return 新身份证的最后一位
 	 */
-	private static char getValidValue(String code) {
-		char[] cs=code.toCharArray();
-		if(cs.length!=17){
-			throw new RuntimeException("获取校验码长度异常");
+	private static String getIdCardValidValue(String code) {
+		int sum = 0;
+		for (int i = 0; i < 17; i++) {
+			sum += Integer.parseInt(StringUtils.substring(code, i, i + 1)) * weight_idcard[i];
 		}
-		int sum=0;
-		for(int i=0;i<cs.length;i++){
-			sum+=modeValue[i]*Integer.parseInt(Character.toString(cs[i]));
-		}
-		int y=sum%11;
-		return validValue[y-1];
+		int check = (12 - sum % 11) % 11;
+		String result = Integer.toString(check);
+		if (check == 10)
+			result = "X";
+		return result;
 	}
 
 	/**
 	 * 根据身份证获得性别
+	 * 
 	 * @param idcode
 	 * @return
 	 */
-	public static String getGender(String idcode){
-		if(!validIdCardCode(idcode)){
-			throw new RuntimeException("身份证号["+idcode+"]错误");
+	public static String getIdCardGender(String idcode) {
+		if (!validIdCardCode(idcode)) {
+			throw new RuntimeException("身份证号[" + idcode + "]错误");
 		}
 		String sexPostion = null;
-		if(idcode.length()==18){
-			 sexPostion = idcode.substring(idcode.length()-2, idcode.length()-1);
-		} else if(idcode.length()==15){
-			 sexPostion = idcode.substring(idcode.length()-1, idcode.length());
+		if (idcode.length() == 18) {
+			sexPostion = idcode.substring(idcode.length() - 2, idcode.length() - 1);
+		} else if (idcode.length() == 15) {
+			sexPostion = idcode.substring(idcode.length() - 1, idcode.length());
 		}
-		if(sexPostion!=null){
-			if(Integer.parseInt(sexPostion)%2==0){//偶数
+		if (sexPostion != null) {
+			if (Integer.parseInt(sexPostion) % 2 == 0) {// 偶数
 				return "女";
-			} else{
+			} else {
 				return "男";
 			}
-		}else{
+		} else {
 			return "未知";
 		}
 	}
+
 	/**
 	 * 通过证件号获取生日信息
-	 *@author luqili 2016年12月19日
+	 * 
+	 * @author luqili 2016年12月19日
 	 * @param code
 	 * @return
 	 */
-	public static Date getBirthday(String code){
-		if(!validPattern.matcher(code).find()){
+	public static Date getIdCardBirthday(String code) {
+		if (!pattern_idcard.matcher(code).find()) {
 			throw new RuntimeException("证件号有误");
 		}
 		try {
-			String birth=StringUtils.substring(code, 6, 14);
-			Date birthDay =DateUtils.parseDate(birth,"yyyyMMdd");
+			String birth = StringUtils.substring(code, 6, 14);
+			Date birthDay = DateUtils.parseDate(birth, "yyyyMMdd");
 			return birthDay;
 		} catch (ParseException e) {
 			throw new RuntimeException("证件号有误,转换日期异常");
 		}
 	}
+
 	/**
 	 * 验证身份证是否正确，X必须为大写 不通过抛出异常信息
-	 *@author luqili 2016年12月19日
+	 * <li>二代身份证校验函数
+	 * 
+	 * @author luqili 2016年12月19日
 	 * @param code
 	 * @return
 	 */
@@ -104,40 +101,106 @@ public class IDCardUtils {
 		if (StringUtils.isEmpty(code)) {
 			throw new RuntimeException("证件号不能为空");
 		}
-		if(!validPattern.matcher(code).find()){
-			//正则表达式不匹配
+		if (!pattern_idcard.matcher(code).find()) {
+			// 正则表达式不匹配
 			throw new RuntimeException("证件号格式错误");
 		}
-		Date birthDay=getBirthday(code);
-		Date now =DateUtils.truncate(new Date(), Calendar.DATE);
-		if(now.getTime()-birthDay.getTime()<0){
+		Date birthDay = getIdCardBirthday(code);
+		Date now = DateUtils.truncate(new Date(), Calendar.DATE);
+		if (now.getTime() - birthDay.getTime() < 0) {
 			throw new RuntimeException("生日日期不能大于当前时间");
 		}
-		Integer age=DateUtils.differYear(now, birthDay);
-		if(age>maxAge){
-			throw new RuntimeException("年龄不能大于"+maxAge+"岁");
+		Integer age = DateUtils.differYear(now, birthDay);
+		if (age > maxAge) {
+			throw new RuntimeException("年龄不能大于" + maxAge + "岁");
 		}
-		String district=StringUtils.substring(code, 0,6);
-		if(!DistrictUtils.containsDistrict(district)){
+		String district = StringUtils.substring(code, 0, 6);
+		if (!DistrictUtils.containsDistrict(district)) {
 			throw new RuntimeException("证件号行政区划有误");
 		}
-		char v=getValidValue(StringUtils.left(code, 17));
-		if(!StringUtils.right(code, 1).equals(Character.toString(v))){
+		String v = getIdCardValidValue(code);
+		if (!StringUtils.right(code, 1).equals(v)) {
 			throw new RuntimeException("校验码错误");
 		}
 		return true;
 	}
+
 	/**
 	 * 验证证件号是否正确
-	 *@author luqili 2016年12月19日
+	 * <li>二代身份证校验函数
+	 * 
+	 * @author luqili 2016年12月19日
 	 * @param code
 	 * @return
 	 */
-	public static boolean validIdCardCode(String code){
-		try{
+	public static boolean validIdCardCode(String code) {
+		try {
 			return validIdCardCodeMessage(code);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	/**
+	 * 统一社会信用代码校验（必须为大写）
+	 * <li>统一社会信用代码校验函数,编码规范见<法人和其他组织统一社会信用代码编码规则>(GB 32100-2015)
+	 * 
+	 * @author luqili 2016年12月20日
+	 * @param code
+	 * @return
+	 */
+	public static boolean vliadCreditCode(String code) {
+		if (StringUtils.isBlank(code)) {
+			return false;
+		}
+		Matcher mt = pattern_creditcode.matcher(code);
+		if (!mt.find()) {
+			return false;
+		}
+		int sum = 0;
+		for (int i = 0; i < 17; i++) {
+			int index = Arrays.binarySearch(tables, code.charAt(i));
+			sum += index * weight_creditcode[i];
+		}
+		int c17 = 31 - sum % 31;
+		if (c17 == 31) {
+			c17 = 0;
+		}
+		return code.charAt(17) == tables[c17];
+	}
+
+	/**
+	 * 验证是否为有效的组织机构代码（必须为大写）
+	 * <li>组织结构代码校验函数,编码规范见<全国组织机构代码编制规则>(GB 11714-1997)
+	 * 
+	 * @author luqili 2016年12月20日
+	 * @param code
+	 * @return
+	 */
+	public static boolean validOrgCode(String code) {
+		if (StringUtils.isBlank(code)) {
+			return false;
+		}
+		Matcher mt = pattern_orgcode.matcher(code);
+		if (!mt.find()) {
+			return false;
+		}
+		int sum = 0;
+		for (int i = 0; i < 8; i++) {
+			int index = Arrays.binarySearch(tables, code.charAt(i));
+			sum += index * weight_orgcode[i];
+		}
+		int c9 = 11 - sum % 11;
+		String v = c9 + "";
+		switch (c9) {
+		case 10:
+			v = "X";
+			break;
+		case 11:
+			v = "0";
+		}
+		String y = StringUtils.right(code, 1);// 最后一位
+		return v.equals(y);
+
 	}
 }
--- myutils/src/test/java/com/luqili/utils/idcard/IDCardUtilsTest.java
+++ myutils/src/test/java/com/luqili/utils/idcard/IDCardUtilsTest.java
@@ -1,27 +1,55 @@
 package com.luqili.utils.idcard;
 
+import java.text.ParseException;
+import java.util.Date;
+
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
+import com.luqili.utils.date.DateUtils;
+
 public class IDCardUtilsTest {
-	private String code="371521199004291410";
-  @Test
-  public void getBirthday() {
-	  IDCardUtils.getBirthday(code);
-  }
+	private String idcard = "331022197904015377";
+	private String creditcode = "811100000673062824";
+	private String orgcode = "G34793367";
 
-  @Test
-  public void getGender() {
-	  IDCardUtils.getGender(code);
-  }
+	@Test
+	public void getIdCardBirthday() {
+		Date date=IDCardUtils.getIdCardBirthday(idcard);
+		try {
+			AssertJUnit.assertEquals(DateUtils.parseDate("19790401", "yyyyMMdd"), date);
+		} catch (ParseException e) {
+			AssertJUnit.fail("日期异常");
+		}
+	}
 
+	@Test
+	public void getIdCardGender() {
+		String g=IDCardUtils.getIdCardGender(idcard);
+		AssertJUnit.assertEquals("男", g);
+	}
 
-  @Test
-  public void validIdCardCode() {
-	  IDCardUtils.validIdCardCode(code);
-  }
+	@Test
+	public void validIdCardCode() {
+		boolean result=IDCardUtils.validIdCardCode(idcard);
+		AssertJUnit.assertEquals(true, result);
+	}
 
-  @Test
-  public void validIdCardCodeMessage() {
-	  IDCardUtils.validIdCardCodeMessage(code);
-  }
+	@Test
+	public void validIdCardCodeMessage() {
+		boolean result=IDCardUtils.validIdCardCodeMessage(idcard);
+		AssertJUnit.assertEquals(true, result);
+	}
+
+	@Test
+	public void validOrgCode() {
+		boolean result=IDCardUtils.validOrgCode(orgcode);
+		AssertJUnit.assertEquals(true, result);
+	}
+
+	@Test
+	public void vliadCreditCode() {
+		boolean result=IDCardUtils.vliadCreditCode(creditcode);
+		AssertJUnit.assertEquals(true, result);
+	}
 }
