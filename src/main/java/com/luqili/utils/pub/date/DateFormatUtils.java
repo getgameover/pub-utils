85e353bb0438a40de818616e23f961e6b5257fb2
luqili <a@luqili.com>
2017-07-21 15:18:31 +0800

    日期格式化

--- src/main/java/com/luqili/utils/pub/date/DateFormatUtils.java
+++ src/main/java/com/luqili/utils/pub/date/DateFormatUtils.java
@@ -1,10 +1,48 @@
 package com.luqili.utils.pub.date;
 
+import java.util.Date;
+
 /**
  * 扩展DateFormatUtils
+ * 
  * @author luqili 2016年12月12日
  *
  */
 public class DateFormatUtils extends org.apache.commons.lang3.time.DateFormatUtils {
-
+  
+  /**
+   * 默认格式化样式
+   * <li>yyyy-MM-dd
+   * 
+   * @param date
+   * @return
+   */
+  public static String formatCnStyle1(Date date) {
+    String f = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
+    return f;
+  }
+  
+  /**
+   * 默认格式化样式
+   * <li>yyyy-MM-dd HH:mm
+   * 
+   * @param date
+   * @return
+   */
+  public static String formatCnStyle2(Date date) {
+    String f = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm");
+    return f;
+  }
+  
+  /**
+   * 默认格式化样式
+   * <li>yyyy-MM-dd HH:mm:ss
+   * 
+   * @param date
+   * @return
+   */
+  public static String formatCnStyle3(Date date) {
+    String f = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+    return f;
+  }
 }
